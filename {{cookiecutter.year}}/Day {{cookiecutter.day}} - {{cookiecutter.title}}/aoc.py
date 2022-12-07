7f9970c70a002861b119bd7635272537ad67a4d7
lgallagher <lewisjgallagher@gmail.com>
2022-12-07 23:25:48 +0000

    :recycle: Formatting changes to python file
    Add two new lines below functions and other sections.

--- {{cookiecutter.year}}/Day {{cookiecutter.day}} - {{cookiecutter.title}}/aoc.py
+++ {{cookiecutter.year}}/Day {{cookiecutter.day}} - {{cookiecutter.title}}/aoc.py
@@ -1,12 +1,15 @@
 """
 Solution for Advent of Code {{cookiecutter.year}} day {{cookiecutter.day}} - https://adventofcode.com/{{cookiecutter.year}}/day/{{cookiecutter.day}}
 """
+
 import os
 from typing import List, Any
 import timeit
 
+
 EXAMPLE_INPUT = '''\
 '''
+
 EXAMPLE_OUTPUT_PART1 = 0
 EXAMPLE_OUTPUT_PART2 = 0
 
@@ -16,14 +19,17 @@ def _parse_input(data: List[str]) -> List[str]:
 
     return data
 
+
 def part_1_solution(data: List[str]) -> Any:
     """Compute solution to puzzle part 1."""
 
     return 0
 
+
 def part_2_solution(data: List[str]) -> Any:
     """Compute solution to puzzle part 2."""
     return 0
+
 
 if __name__ == "__main__":
     # Compute puzzle with example data
